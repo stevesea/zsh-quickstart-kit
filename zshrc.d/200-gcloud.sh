39db6894fdb11ed08062e478487530e1d7c0fef3
steve christensen <stevec1973@gmail.com>
2022-02-16 16:34:10 -0700

    add gcloud

---
+++ zshrc.d/200-gcloud.sh
@@ -0,0 +1,6 @@
+
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/Users/schristensen/google-cloud-sdk/path.zsh.inc' ]; then . '/Users/schristensen/google-cloud-sdk/path.zsh.inc'; fi
+
+# The next line enables shell command completion for gcloud.
+if [ -f '/Users/schristensen/google-cloud-sdk/completion.zsh.inc' ]; then . '/Users/schristensen/google-cloud-sdk/completion.zsh.inc'; fi
